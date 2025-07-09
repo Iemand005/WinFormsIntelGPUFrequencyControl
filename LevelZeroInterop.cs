a87c7cf5eff97010cf076851775bb3fc43054c23
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-07-09 03:26:39 +0200

    Fix struct

--- LevelZeroInterop.cs
+++ LevelZeroInterop.cs
@@ -9,18 +9,49 @@ namespace LevelZero
         public double Max;
     }
 
-    [StructLayout(LayoutKind.Sequential)]
+    //[StructLayout(LayoutKind.Sequential)]
+    //public struct FrequencyProperties
+    //{
+    //    public uint stype;
+    //    public IntPtr nextPointer;
+    //    public int type;
+    //    public bool onSubdevice;
+    //    //public ulong subdeviceId;
+    //    public bool canControl;
+    //    //public bool isThrottleEventSupported;
+    //    public double min;
+    //    public double max;
+    //}
+
+    public enum ze_structure_type_t : uint
+    {
+        ZE_STRUCTURE_TYPE_FREQ_PROPERTIES = 0x1001
+    }
+
+    public enum zes_freq_domain_t : int
+    {
+        ZES_FREQ_DOMAIN_GPU = 0,
+        ZES_FREQ_DOMAIN_MEMORY = 1
+    }
+
+    public enum ze_bool_t : uint
+    {
+        ZE_BOOL_FALSE = 0,
+        ZE_BOOL_TRUE = 1
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 56)]
     public struct FrequencyProperties
     {
-        public uint stype;
-        public IntPtr nextPointer;
-        public int type;
-        public bool onSubdevice;
-        //public ulong subdeviceId;
-        public bool canControl;
-        //public bool isThrottleEventSupported;
-        public double min;
-        public double max;
+        [FieldOffset(0)] public ze_structure_type_t stype;
+        [FieldOffset(4)] public IntPtr pNext;
+        [FieldOffset(8)] public zes_freq_domain_t type;
+        [FieldOffset(12)] public ze_bool_t onSubdevice;
+        [FieldOffset(16)] public uint subdeviceId;
+        [FieldOffset(20)] public ze_bool_t canControl;
+        [FieldOffset(24)] public ze_bool_t isThrottleEventSupported;
+        [FieldOffset(32)] public double min;
+        [FieldOffset(40)] public double max;
     }
 
     public struct DriverHandle { public IntPtr Handle; }
