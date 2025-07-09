591be17d4746ced35afed1058537286a36a03f58
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-07-09 03:21:44 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WinFormsIntelGPUFrequencyControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            trackBar2 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            trackBar1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar1.Location = new Point(55, 9);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(733, 56);
+            trackBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Max";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Min";
+            // 
+            // trackBar2
+            // 
+            trackBar2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar2.Location = new Point(55, 71);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(733, 56);
+            trackBar2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(trackBar2);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar trackBar1;
+        private Label label1;
+        private Label label2;
+        private TrackBar trackBar2;
+    }
+}
---
+++ LevelZeroInterop.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LevelZero
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FrequencyRange
+    {
+        public double Min;
+        public double Max;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FrequencyProperties
+    {
+        public uint stype;
+        public IntPtr nextPointer;
+        public int type;
+        public bool onSubdevice;
+        //public ulong subdeviceId;
+        public bool canControl;
+        //public bool isThrottleEventSupported;
+        public double min;
+        public double max;
+    }
+
+    public struct DriverHandle { public IntPtr Handle; }
+    public struct DeviceHandle { public IntPtr Handle; }
+    public struct FrequencyHandle { public IntPtr Handle; }
+
+    internal class LevelZeroInterop
+    {
+        [DllImport("ze_loader.dll", EntryPoint = "zeInit")]
+        public static extern int Init(int flags = 0);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zeDriverGet", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetDriver(ref uint count, [In, Out, Optional] DriverHandle[]? driverHandles);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zeDeviceGet", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetDevice(DriverHandle driverHandle, ref uint count, [In, Out, Optional] DeviceHandle[]? deviceHandles);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zesDeviceEnumFrequencyDomains", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int EnumDeviceFrequencyDomains(DeviceHandle deviceHandle,  ref uint count, [In, Out] FrequencyHandle[] frequencyHandles);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zesFrequencyGetProperties", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetFrequencyProperties(FrequencyHandle frequencyHandle, ref FrequencyProperties properties);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zesFrequencyGetRange")]
+        public static extern int GetFrequencyRange(FrequencyHandle frequencyHandle, ref FrequencyRange limits);
+
+        [DllImport("ze_loader.dll", EntryPoint = "zesFrequencySetRange")]
+        public static extern int SetFrequencyRange(FrequencyHandle frequencyHandle, ref FrequencyRange limits);
+    }
+}
